48ef6edc639f704de3128d79cd44b61c9134dda4
beatrizjardim031 <beatrizreisjardim@gmail.com>
2026-04-09 13:49:13 -0500

    Initial commit

---
+++ src/main/java/com/pluralsight/VariableApp.java
@@ -0,0 +1,35 @@
+package com.pluralsight;
+
+public class VariableApp {
+    public static void main(String[] args) {
+        // Question 1:
+        // declare variables here
+        // then code solution
+        // then use System.out.println() to display results
+        // ex: System.out.println("The answer is " + answer);
+        // REPEAT FOR NEXT EXERCISE
+
+        // step 1
+        String favoriteColor = "green";
+        System.out.printf("My favorite color is %s.%n", favoriteColor);
+
+        int startingYear = 2026;
+        System.out.printf("I started year up in %d.%n", startingYear);
+
+        char middleInitial = 'R';
+        System.out.printf("My middle initial is %c.%n", middleInitial);
+
+        String havePets = "Yes";
+        System.out.printf("Do I have pets? %s.%n", havePets);
+
+        String message = "Have a nice day!";
+        System.out.println(message);
+
+        // step 2
+
+
+    }
+
+
+
+}
